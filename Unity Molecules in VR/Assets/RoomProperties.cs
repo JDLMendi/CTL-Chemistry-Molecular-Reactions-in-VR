749b119910b0fe1d76bf3c4ec13bd66f7bc45927
Jhundon De Leon Mendi <jhundon.mendi21@gmail.com>
2025-10-17 10:22:36 +0100

    Removed old project

--- Unity Molecules in VR/Assets/RoomProperties.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class RoomProperties : MonoBehaviour
-{
-    [Header("Room Properties")] 
-    public string hostID = "hostID";
-    public string isPeersVisible = "isPeersVisible";
-}
--- Unity Molecules in VR/Assets/Scripts/Readme.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Readme : MonoBehaviour
-{
-    [TextArea(3, 40)]
-    public string readme;
-}
-
